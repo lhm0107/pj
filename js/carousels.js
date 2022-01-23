8ce1b37feb971a425f36a94d9fdc571bddb56b77
DESKTOP-N4V80RO\lhm0107 <lhm820107@gmail.com>
2022-01-23 21:41:08 +0900

    🚧 productCarousel change name to carousels.js

--- js/carousels.js
+++ js/carousels.js
@@ -9,4 +9,22 @@ const productCarousel = tns({
 	autoplayButtonOutput: false,
 	mouseDrag: true,
 	preventScrollOnTouch: true,
-})
+});
+
+const userGallery = tns({
+	container: '.user-gallery .slider-list',
+	controls: false,
+	gutter: 4,
+	edgePadding: 16,
+	loop: false,
+	arrowKeys: true,
+	mouseDrag: true,
+	preventScrollOnTouch: true,
+	responsive: {
+		768: {
+			controls: true,
+			gutter: 6,
+			edgePadding: 75,
+		},
+	},
+});
