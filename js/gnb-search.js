dbc47c0b854c1e72878dbd90cd141ea232ce4821
DESKTOP-9PI9TTC\a <lhm820107@gmail.com>
2022-02-25 13:38:24 +0900

    💄 add gnb-search-history delete and emtpy messege effect

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,21 +1,35 @@
 const gnbSearch = document.querySelector('.gnb-search');
 const gnbSearchInput = gnbSearch.querySelector('input');
 const gnbSearchHistory = gnbSearch.querySelector('.search-history');
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol');
+const gnbSearchHistorylistBlank = gnbSearchHistory.querySelector('.search-history-content.is-blank');
+
+const deleteAllbutton = gnbSearchHistory.querySelector('.search-history-header button');
+
 
 function closeGnbSearchHistoryOnClickingOutside(e) {
 	if (!gnbSearch.contains(e.target)) {
-		window.removeEventListener('ckick', closeGnbSearchHistoryOnClickingOutside);
 		gnbSearchHistory.classList.remove('is-active');
+		window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside);
 	}
 }
 
-function openGnbSearchHistoryOpen() {
+function openGnbSearchHistory() {
+	if(gnbSearchHistoryList.children.length === 0) {
+		gnbSearchHistorylistBlank.classList.remove('is-blank');
+	}
 	if (!gnbSearch.classList.contains('is-active')) {
-		window.addEventListener('ckick', closeGnbSearchHistoryOnClickingOutside);
-		gnbSearchHistory.classList.add('is-active');
+		window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside);
 	}
+	gnbSearchHistory.classList.add('is-active');
 }
 
-gnbSearchInput.addEventListener('focus', openGnbSearchHistoryOpen);
+function deleteAllSearchHistoryItems(){
+	gnbSearchHistoryList.innerHTML = '';
+	gnbSearchHistory.classList.remove('is-active')
+}
 
-window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside);
+gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
+
+deleteAllbutton.addEventListener('click',deleteAllSearchHistoryItems );
+
