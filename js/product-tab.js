cb5489aeab8028c4fde854a066b605e3d389afa5
DESKTOP-N4V80RO\lhm0107 <lhm820107@gmail.com>
2022-02-27 23:09:47 +0900

    💄 product-tab move active effect update

--- js/product-tab.js
+++ js/product-tab.js
@@ -5,13 +5,21 @@ const TOP_HEADER_DESKTOP = 80 + 50 + 54;
 const TOP_HEADER_MOBILE = 50 + 40 + 40;
 
 let currentActiveTab = productTab.querySelector('.is-active');
+let disableUpdating = false;
 
 function toggleActiveTab() {
 	const tabItem = this.parentNode;
 	if (currentActiveTab !== tabItem) {
+		disableUpdating = true;
 		tabItem.classList.add('is-active');
-		currentActiveTab.classList.remove('is-active');
+		if (currentActiveTab !== null) {
+			currentActiveTab.classList.remove('is-active');
+		}
 		currentActiveTab = tabItem;
+
+		setTimeout(() => {
+			disableUpdating = false;
+		}, 1000);
 	}
 }
 
@@ -30,3 +38,61 @@ productTabButtonList.forEach((button) => {
 	button.addEventListener('click', toggleActiveTab);
 	button.addEventListener('click', scrollToTabPanel);
 });
+
+const productTabPanelIdList = ['product-spec', 'product-review', 'product-inquiry', 'product-shipment', 'product-recommendation'];
+const productTabPanelList = productTabPanelIdList.map((panelID) => {
+	const tabPanel = document.querySelector(`#${panelID}`);
+	return tabPanel;
+});
+
+const productTabPanelPositionMap = {};
+
+function detecTabPanelPosition() {
+	productTabPanelList.forEach((panel) => {
+		const id = panel.getAttribute('id');
+		const position = window.scrollY + panel.getBoundingClientRect().top;
+		productTabPanelPositionMap[id] = position;
+	});
+
+	updateActiveTabOnScroll();
+}
+
+function updateActiveTabOnScroll() {
+	if (disableUpdating) {
+		return;
+	}
+	const scrolledAmount = window.scrollY + (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP + 80 : TOP_HEADER_MOBILE + 8);
+
+	let newActiveTab;
+	if (scrolledAmount >= productTabPanelPositionMap['product-recommendation']) {
+		newActiveTab = productTabButtonList[4];
+	} else if (scrolledAmount >= productTabPanelPositionMap['product-shipment']) {
+		newActiveTab = productTabButtonList[3];
+	} else if (scrolledAmount >= productTabPanelPositionMap['product-inquiry']) {
+		newActiveTab = productTabButtonList[2];
+	} else if (scrolledAmount >= productTabPanelPositionMap['product-review']) {
+		newActiveTab = productTabButtonList[1];
+	} else {
+		newActiveTab = productTabButtonList[0];
+	}
+
+	const bodyHeight = document.body.offsetHeight + (innerHeight < 1200 ? 56 : 0);
+	if (window.scrollY + window.innerHeight === bodyHeight) {
+		newActiveTab = productTab[4];
+	}
+
+	if (newActiveTab) {
+		newActiveTab = newActiveTab.parentNode;
+		if (newActiveTab !== currentActiveTab) {
+			newActiveTab.classList.add('is-active');
+			if (currentActiveTab !== null) {
+				currentActiveTab.classList.remove('is-active');
+			}
+			currentActiveTab = newActiveTab;
+		}
+	}
+}
+
+window.addEventListener('load', detecTabPanelPosition);
+window.addEventListener('resize', detecTabPanelPosition);
+window.addEventListener('scroll', updateActiveTabOnScroll);
